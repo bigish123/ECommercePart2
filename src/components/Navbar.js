fe61f08a925abd6f2eb1ee8c1b3f8b1ee6c946da
bigish123 <102987684+bigish123@users.noreply.github.com>
2022-07-24 23:29:41 -0400

    new project

---
+++ src/components/Navbar.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react'
+import {Link} from 'react-router-dom';
+import logo from '../logo.svg';
+import styled from 'styled-components';
+import {ButtonContainer} from './Button';
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <NavWrapper className="navbar nav-bar-expand-sm navbar-dark px-sm-5">
+               <Link to='/'>
+                  <img src={logo} alt="store" className="navbar-brand" />
+               </Link> 
+               <ul className="navbar-nav">
+                 <li className="nav-item ml-5">
+                    <Link to="/" className="nav-link">
+                       About Us
+                    </Link>
+                 </li>
+                 <li className="nav-item ml-5">
+                    <Link to="/" className="nav-link">
+                       Products
+                    </Link>
+                 </li>
+                 <li className="nav-item ml-5">
+                    <Link to="/" className="nav-link">
+                       Contact Us
+                    </Link>
+                 </li>
+                 <li className="nav-item ml-5">
+                    <Link to="/" className="nav-link">
+                       Login 
+                    </Link>
+                 </li>
+                 <li className="nav-item ml-5">
+                    <Link to="/" className="nav-link">
+                       Register
+                    </Link>
+                 </li>
+               </ul>
+               <Link to="/cart" className="ml-auto">
+                  <ButtonContainer>
+                      <i className="fas fa-cart-plus">my cart</i>
+                  </ButtonContainer>
+               </Link>
+            </NavWrapper>
+        )
+    }
+}
+const NavWrapper = styled.nav `
+background:var(--mainBlue);
+.nav-link{
+    color:var(--mainWhite) !important;
+    font-size:1.3 rem;
+    text-transform:capitalize;
+}
+ul{
+    display: flex;
+    position: center;
+    margin: 10px;
+    flex-direction: row;
+  justify-content: center;
+}
+`;
